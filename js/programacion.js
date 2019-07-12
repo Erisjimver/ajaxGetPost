0ac84ea1aab6979689305d5e28f05ca7464c4962
Israel_Jimenez <41273721+Erisjimver@users.noreply.github.com>
2019-07-11 19:42:06 -0500

    Validar usuario usando Ajax

--- js/programacion.js
+++ js/programacion.js
@@ -1,5 +1,25 @@
 
 $(document).ready(function() {
 
-			
+	$("#login").submit(function(){
+
+		var datosFormulario={
+							usuario:$("#usuario").val(),
+							contra:$("#contra").val()
+							}
+		$.get("login.php",datosFormulario,procesarDatos);
+
+		return false;
+	});
+
+	function procesarDatos(datos_devueltos){
+
+		if(datos_devueltos=="autorizado"){
+			$("#contenidos_externos").html("<p>Usuario correcto. Bienvenido de nuevo </p>");
+		}else{
+			$("#contenidos_externos").html("<p>Usuario no autorizado. </p>");
+		}
+
+	}
+
 });
